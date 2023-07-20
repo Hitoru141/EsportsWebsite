7aab4869aee613d2b87532ca278618eaf8d424e6
Steve Bryan Suico <ssuico78@gmail.com>
2023-07-20 18:31:17 +0800

    added carousel admin options -file upload

--- src/Views/Admin/AdCarousel.jsx
+++ src/Views/Admin/AdCarousel.jsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
+import CarouselCards from "./CarouselCards";
+
 const AdCarousel = () => {
-  return <div>AdCarousel</div>;
+  const [images, setImages] = useState([]);
+
+  const handleFileChange = (event) => {
+    const fileList = event.target.files;
+    const selectedImages = [];
+
+    // Loop through the uploaded files and push them to the selectedImages array
+    for (let i = 0; i < fileList.length; i++) {
+      selectedImages.push(URL.createObjectURL(fileList[i]));
+    }
+
+    setImages(selectedImages);
+  };
+
+  return (
+    <div className="adcarousel-wrap">
+      <form className="form1">
+        <span className="form-title">
+          Maximum {images.length} carousel images
+        </span>
+        <p className="form-paragraph">File should be an image</p>
+        <label htmlFor="file-input" className="drop-container">
+          <span className="drop-title">Drop files here</span>
+          or
+          <input
+            type="file"
+            accept="image/*"
+            required
+            id="file-input"
+            multiple
+            onChange={handleFileChange}
+          />
+        </label>
+      </form>
+      <div className="previmg-wrap">
+        {[...Array(images.length)].map((_, index) => (
+          <CarouselCards key={index} carsel={images[index] || null} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AdCarousel;
---
+++ src/Views/Admin/CarouselCards.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line react/prop-types
+const CarouselCards = ({ carsel }) => {
+  return (
+    <div className="carousel-card">
+      <img src={carsel} className="carousel-card" />
+    </div>
+  );
+};
+
+export default CarouselCards;
