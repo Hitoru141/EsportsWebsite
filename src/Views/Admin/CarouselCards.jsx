b2c54afb7892cd152ddef1cc8ac3d7c9006a8971
Steve Bryan Suico <ssuico78@gmail.com>
2023-07-22 11:12:39 +0800

    carouselopts

--- src/Views/Admin/CarouselCards.jsx
+++ src/Views/Admin/CarouselCards.jsx
@@ -1,8 +1,24 @@
+import "../../Styles/admin.css";
+
 // eslint-disable-next-line react/prop-types
 const CarouselCards = ({ carsel }) => {
+  const handleDelete = () => {
+    // Show a confirmation message before proceeding with the delete action
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this item?"
+    );
+    if (confirmDelete) {
+      console.log("Delete button clicked!");
+      // You can add your delete logic here
+    }
+  };
+
   return (
     <div className="carousel-card">
       <img src={carsel} className="carousel-card" />
+      <button className="delete-button" onClick={handleDelete}>
+        Delete
+      </button>
     </div>
   );
 };
