a1f819c351cd5b15fba77b00d16f6d13f680da17
tylerbert31 <tbaring_ccs@uspf.edu.ph>
2023-09-02 16:47:03 +0800

    Update postTeam.jsx

--- src/Views/Admin/Teams/Addteam/postTeam.jsx
+++ src/Views/Admin/Teams/Addteam/postTeam.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { appSettings } from "../../../../Appdata/appdata";
 
 function submitTeam(teamName, teamBannerURL, teamLogoURL) {
   axios
-    .post("https://astraeus-firebase-endpoints.onrender.com/teams", {
+    .post(appSettings.teams, {
       teamName: teamName,
       teamBannerURL: teamBannerURL,
       teamLogoURL: teamLogoURL,
@@ -11,13 +12,13 @@ function submitTeam(teamName, teamBannerURL, teamLogoURL) {
       if (response.status === 201) {
         // Team Added Successfully
         window.alert("Team Added Successfully");
-      } else if (response.status === 409) {
-        // Alert teamName exists
-        window.alert("Team Name already exists");
       }
     })
     .catch((error) => {
-      if (error.response && error.response.status === 500) {
+      if (response.status === 409) {
+        // Alert teamName exists
+        window.alert("Team Name already exists");
+      } else if (error.response && error.response.status === 500) {
         // Internal Server Error
         window.alert("Internal Server Error");
       } else {
