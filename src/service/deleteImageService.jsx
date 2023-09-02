533f0bc63c4ef80c03336702862e19844f395b91
marlonG <mgacrama_ccs@uspf.edu.ph>
2023-09-02 16:03:20 +0800

    Delete of image from storage Done

--- src/service/deleteImageService.jsx
+++ src/service/deleteImageService.jsx
@@ -1,15 +1,9 @@
 import { getStorage, ref, deleteObject } from "firebase/storage";
 
-const deleteImageHandler = (folderName, imageURL) => {
-  console.log(folderName);
-  console.log(imageName);
-  const imageName = imageURL.split("/").pop();
-  console.log(imageName);
-
-  //   console.log(team.teamBannerURL);
-  //   const storage = getStorage();
-  //   const deleteImage = ref(storage, `${folderName}/${imageName}`);
-  //   console.log(deleteImage);
+const deleteImageHandler = (imageURL) => {
+  const storage = getStorage();
+  const deleteImage = ref(storage, imageURL);
+  deleteObject(deleteImage);
 };
 
 export default deleteImageHandler;
--- src/service/teamasHandler/deleteTeamHandler.jsx
+++ src/service/teamasHandler/deleteTeamHandler.jsx
@@ -1,24 +1,20 @@
 import axios from "axios";
 import { appSettings } from "../../Appdata/appdata";
-import { getStorage, ref, deleteObject } from "firebase/storage";
 import deleteImageHandler from "../deleteImageService";
 
 const deleteTeamHandler = (team) => {
-  const folderName = "carousel";
-  const imageURL = team.teamLogoURL;
-  deleteImageHandler(folderName, imageURL);
-  //   console.log(team.teamBannerURL);
-  //   const storage = getStorage();
-  //   const deleteImage = ref(storage, team.teamBannerURL);
-  axios
-    .delete(`${appSettings.teams}/${team.teamName}`)
-    .then((response) => {
+  const { teamLogoURL, teamBannerURL } = team;
+
+  try {
+    axios.delete(`${appSettings.teams}/${team.teamName}`).then((response) => {
       alert("Deleted from Firebase Firestore/Storage successfully!");
       window.location.reload();
-    })
-    .catch((error) => {
-      console.log(error);
     });
+    deleteImageHandler(teamLogoURL);
+    deleteImageHandler(teamBannerURL);
+  } catch (err) {
+    alert(`Unable to delete Team`);
+  }
 };
 
 export default deleteTeamHandler;
