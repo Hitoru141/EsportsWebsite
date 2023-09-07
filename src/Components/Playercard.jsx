d2c492964acf3c7ea87d02b93ad8da76157fd452
Steve Bryan Suico <ssuico78@gmail.com>
2023-09-07 13:16:44 +0800

    player card adjustments

--- src/Components/Playercard.jsx
+++ src/Components/Playercard.jsx
@@ -1,5 +1,5 @@
 import "../Styles/players.css";
-
+import samp from "../assets/CEOprof3.jpg";
 import { BsTwitch } from "react-icons/bs";
 import { BsFacebook } from "react-icons/bs";
 import { BsDiscord } from "react-icons/bs";
@@ -7,9 +7,15 @@ import { BsDiscord } from "react-icons/bs";
 const Playercard = () => {
   return (
     <div className="img-card">
+      <img src={samp} className="img-card-prof" />
       <div className="hover-content">
         <div className="text-container">
-          <p>HITORU</p>
+          <p className="player-name">HITORU</p>
+          <div className="player-type-box">
+            <p className="player-type">PLAYER</p>
+          </div>
+
+          <p className="player-type">CEBU, PHL</p>
         </div>
 
         <div className="button-container">
