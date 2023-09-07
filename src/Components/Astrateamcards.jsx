96100477585e38ebc84d28b6dfaff97824f8d355
Steve Bryan Suico <ssuico78@gmail.com>
2023-09-07 11:42:41 +0800

    fixed valoteam css

--- src/Components/Astrateamcards.jsx
+++ src/Components/Astrateamcards.jsx
@@ -1,9 +1,10 @@
 // eslint-disable-next-line react/prop-types
-function Astrateamcards({ teamlogo }) {
+function Astrateamcards({ teamlogo, tname }) {
   return (
     <div className="b-game-card">
       <div className="b-game-card__cover">
         <img src={teamlogo} className="b-game-card__cover" />
+        <p>{tname}</p>
       </div>
     </div>
   );
