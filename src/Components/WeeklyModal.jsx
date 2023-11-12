3076ceb26f359c279e60a0e409cec1d7af8d183e
Steve Bryan Suico <ssuico78@gmail.com>
2023-11-12 12:51:06 +0800

    installed new qr

--- src/Components/WeeklyModal.jsx
+++ src/Components/WeeklyModal.jsx
@@ -1,52 +1,43 @@
-// import React, { useEffect, useRef, useState } from "react";
-// import QRCode from "qrcode";
+import React, { useEffect, useRef, useState } from "react";
+import QRCode from "qrcode.react";
 
-// const WeeklyModal = ({ closeModal }) => {
-//   const qrCodeRef = useRef();
-//   const [link, setLink] = useState("https://www.google.com/forms/about/");
+const WeeklyModal = ({ closeModal }) => {
+  const [link, setLink] = useState("https://www.google.com/forms/about/");
 
-//   useEffect(() => {
-//     QRCode.toDataURL(link, (err, url) => {
-//       qrCodeRef.current.src = url;
-//     });
-//   }, [link]);
+  const handleInputChange = (e) => {
+    setLink(e.target.value);
+  };
 
-//   const copyToClipboard = async () => {
-//     try {
-//       await navigator.clipboard.writeText(link);
-//       alert("Link copied to clipboard!");
-//     } catch (err) {
-//       console.error("Failed to copy text: ", err);
-//     }
-//   };
+  return (
+    <div className="weekly-modal-overlay">
+      <div className="weekly-modal">
+        <div className="exit_btn" onClick={closeModal}>
+          X
+        </div>
+        <h2 className="h2-weekly">Copy link or scan using the QR code below</h2>
+        <div className="input-group-weekly">
+          <input
+            type="text"
+            className="weekly-input"
+            placeholder="uiverse@verse.io"
+            value={link}
+            onChange={handleInputChange}
+          />
+          <button
+            className="weeklybtn-submit"
+            onClick={() => {
+              // You can handle copying to clipboard here if needed
+              alert("Link copied to clipboard!");
+            }}
+          >
+            Copy link
+          </button>
+        </div>
+        {/* Use qrcode.react component with value prop for generating QR code */}
+        <QRCode value={link} className="weekly-qr" />
+      </div>
+    </div>
+  );
+};
 
-//   const handleInputChange = (e) => {
-//     setLink(e.target.value);
-//   };
-
-//   return (
-//     <div className="weekly-modal-overlay">
-//       <div className="weekly-modal">
-//         <div className="exit_btn" onClick={closeModal}>
-//           X
-//         </div>
-//         <h2 className="h2-weekly">Copy link or scan using the QR code below</h2>
-//         <div className="input-group-weekly">
-//           <input
-//             type="text"
-//             className="weekly-input"
-//             placeholder="uiverse@verse.io"
-//             value={link}
-//             onChange={handleInputChange}
-//           />
-//           <button className="weeklybtn-submit" onClick={copyToClipboard}>
-//             Copy link
-//           </button>
-//         </div>
-//         <img ref={qrCodeRef} alt="QR Code" className="weekly-qr" />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default WeeklyModal;
+export default WeeklyModal;
