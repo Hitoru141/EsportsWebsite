a9712f1bdba2bb21816453f04b74238cb7fdba97
Steve Bryan Suico <ssuico78@gmail.com>
2023-11-12 13:03:10 +0800

    removed react-typewriter

--- src/Views/Careers.jsx
+++ src/Views/Careers.jsx
@@ -3,28 +3,15 @@ import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import tbanner from "../assets/Astraeus.jpg";
 
-import TypeWriterEffect from "react-typewriter-effect";
-
 const Careers = () => {
   return (
     <div className="glbwrapper">
       <Navbar banner={tbanner} />
 
-      <div className="h1-weekly">
-        Nothing to see here for now...
-        <TypeWriterEffect
-          textStyle={{
-            color: "#8a2be2",
-            fontWeight: 500,
-            fontSize: "1.5em",
-          }}
-          startDelay={1000}
-          cursorColor="#8a2be2"
-          multiText={["{Under Development}"]}
-          multiTextDelay={1000}
-          typeSpeed={100}
-        />
-      </div>
+      <p className="h1-weekly">
+        Nothing to see here for now...{" "}
+        <span className="h-esport">&#10100;Under Development&#10101;</span>
+      </p>
 
       <Footer />
     </div>
