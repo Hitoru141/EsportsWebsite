0cb71f052f864de37f5258ac0157e8e9839a65cc
Steve Bryan Suico <ssuico78@gmail.com>
2023-11-12 13:51:55 +0800

    fix career page

--- src/Views/Careers.jsx
+++ src/Views/Careers.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import tbanner from "../assets/Astraeus.jpg";
+import prog from "../assets/progresss.gif";
 
 const Careers = () => {
   return (
@@ -9,10 +10,12 @@ const Careers = () => {
       <Navbar banner={tbanner} />
 
       <p className="h1-weekly">
-        Nothing to see here for now...{" "}
+        Nothing to see here for now...
         <span className="h-esport">&#10100;Under Development&#10101;</span>
       </p>
-
+      <div className="career-card">
+        <img src={prog} className="career-card-img" />
+      </div>
       <Footer />
     </div>
   );
